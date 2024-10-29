d53c42a7591e1e7bba8c37e9960d8114b93b9126
Kilyan Poilly <kpoilly@made-f0Br3s11.clusters.42paris.fr>
2024-10-29 15:10:26 +0100

    yoyoyo

--- includes/ft_irc.hpp
+++ includes/ft_irc.hpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:08:36 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/10/29 14:36:24 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/10/29 15:10:20 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_IRC_HPP
 
 # include <iostream>
-# include <string>
+# include <cstring>
 # include <vector>
 # include <sys/socket.h>
 # include <sys/types.h>
