b2e83bc6631eee345596f99a2844726b4460030f
Kilyan Poilly <kpoilly@paul-f4Br1s2.clusters.42paris.fr>
2024-11-11 16:15:45 +0100

    structure

--- srcs/user.cpp
+++ srcs/user.cpp
@@ -6,8 +6,19 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:31:27 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/11/11 14:31:36 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/11/11 16:14:32 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/user.hpp"
+
+User::User(){};
+User::User(const User& copy){*this = copy;};
+
+User& User::operator=(const User& copy)
+{
+	this->_name = copy._name;
+	this->_ipAddress = copy._ipAddress;
+};
+
+User::~User(){};
