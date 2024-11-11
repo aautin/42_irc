9c31d71223738a419571efc41749391ffd9753f0
Kilyan Poilly <kpoilly@paul-f4Br1s2.clusters.42paris.fr>
2024-11-11 15:42:47 +0100

    structure

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,10 +6,28 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:30:58 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/11/11 15:35:02 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/11/11 15:42:39 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/server.hpp"
 
 Server::Server(): _port(0){};
+Server::Server(const Server& copy){*this = copy;};
+
+Server& Server::operator=(const Server& copy)
+{
+	this->_port = copy._port;
+	this->_password = copy._password;
+	this->_channels_list = copy._channels_list;
+	this->_users_list = copy._users_list;
+};
+
+Server::~Server()
+{
+	for (size_t i = 0; i < this->_channels_list.size(); i++)
+		delete this->_channels_list[i];
+	
+	for (size_t i = 0; i < this->_users_list.size(); i++)
+		delete this->_users_list[i];
+};
